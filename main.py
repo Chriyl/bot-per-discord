d23aa514b8f620730bad4c20b2cb07d9c40add3d
Christian Montanaro <christianm0602@outlook.it>
2023-08-09 14:56:55 +0200

    add chi sono fun

--- main.py
+++ main.py
@@ -14,15 +14,22 @@ load_dotenv(dotenv_path=r"C:\Users\chris\OneDrive\Desktop\cred\credentials.env")
 TOKEN = os.environ.get("TOKEN") # qui prende il token come variabile d'ambiente
 
 
-client = commands.Bot(command_prefix="!", intents=intents) # specifici l'intenti del bot
+bot = commands.Bot(command_prefix="!", intents=intents) # specifici l'intenti del bot
 
 
-@client.event # questo é il messaggio che esce in console se il bot va online
+@bot.event # questo é il messaggio che esce in console se il bot va online
 async def on_ready():
-    print(f'Bot is ready. Logged in as {client.user.name}')
+    print(f'Bot is ready. Logged in as {bot.user.name}')
 
-@client.command() # questo é il ping del bot
+@bot.command() # questo é il ping del bot
 async def ping(ctx): # ctx é l'argomento di colui che ha mandato il messaggio ed é un oggetto quindi ha metodi e elementi
     await ctx.send("pong")
 
-client.run(TOKEN) # qui il bot runna
+@bot.command()
+async def chiSono(ctx):
+    author: str = ctx.author
+    channel: str = ctx.channel
+    await ctx.reply(f"sei {author.mention} e stai scrivendo in {channel}")
+    
+
+bot.run(TOKEN) # qui il bot runna
