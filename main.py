6c8b6f418d38f0e85a9c482d82ba02b7a3fffcfe
Christian Montanaro <christianm0602@outlook.it>
2023-10-07 14:55:53 +0200

    added help

--- main.py
+++ main.py
@@ -27,43 +27,45 @@ async def on_ready() -> None:
 
 
 
-@bot.command() # questo é il ping del bot
+@bot.command(name='ping', help='sanity check') # questo é il ping del bot
 async def ping(ctx)  -> None: # ctx é l'argomento di colui che ha mandato il messaggio ed é un oggetto quindi ha metodi e elementi
     await ctx.send("pong")
 
-@bot.command()
+@bot.command(name='chiSono', help='ti dice chi sei e in che canale stai scrivendo')
 async def chiSono(ctx) -> None:
     author: str = ctx.author.mention
     channel: str = ctx.channel
     await ctx.reply(f"sei {author} e stai scrivendo in {channel}")
 
-@bot.command()
+@bot.command(name='gatto', help='invia una foto di un gatto')
 async def gatto(ctx) -> None:
     await ctx.send(file = utils.prendiFoto())
 
-@bot.command()
+@bot.command(name='insulto', help='invia un insulto dalla pull standard')
 async def frase(ctx, arg: str) -> None:
 
     frase: str = utils.prendiFrase(PATH_TEXT)
     frase = utils.formattaInsulto(frase, arg)
     await ctx.send(frase)
 
-@bot.command()
+@bot.command(name='Sorgente', help='invia il codice sorgente')
 async def Sorgente(ctx) -> None:
     ctx.send(r"https://github.com/Chriyl/bot-per-discord")
 
-@bot.command()
+@bot.command(name='Norris', help='invia una citazione di chuck norris')
 async def Norris(ctx) -> None:
     response: dict[str:] = utils.JsonResponseHandler("https://api.chucknorris.io/jokes/random")
     cit = response['value']
     cit = utils.traduci(cit)
     await ctx.send(cit)
-@bot.command()
+@bot.command(name='Noia', help='consigli un pó cringe per quando ti annoi')
 async def Noia(ctx) -> None:
     response: dict[str:] = utils.JsonResponseHandler("https://www.boredapi.com/api/activity/")
     cit: str = response['activity']
     cit = utils.traduci(cit)
     await ctx.send(cit)
+
+bot/
 
 
 bot.run(TOKEN) # qui il bot runna
